70635ebb3e9ab819bfd89cec7eb76cbb257639b5
Syed Safwan Abbas <safwanabbas2020@gmail.com>
2023-11-08 22:44:11 +0500

    TalentHunt Jobs App (REACT NATIVE)

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
+
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
+import styles from "./welcome.style";
+const jobTypes = ["Full-time", "Part-time", "Contract"];
+
+const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState("Full-Time");
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, Syed</Text>
+        <Text style={styles.welcomeMessage}>Find Best Job For You.</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMethod="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
---
+++ hook/useFetch.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: { ...query },
+    headers: {
+      "X-RapidAPI-Key": "979ef49584msh0c40f43733290e2p111861jsn1e5b8d23bb6a",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.request(options);
+      console.log("Response ", res);
+      setData(res?.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log("Error:", error.message);
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
